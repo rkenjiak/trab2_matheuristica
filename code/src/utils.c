563db89db06ba9f2f2d8274a41f70fc1eb206dc0
Rodrigo <kobayashi.kenji.r@gmail.com>
2024-06-19 20:35:32 -0400

    indice

--- code/src/utils.c
+++ code/src/utils.c
@@ -447,7 +447,7 @@ void printSol(SCIP* scip, char* outputname)
        solval = SCIPgetSolVal(scip, bestSolution, vars[v]);
        if( solval > EPSILON )
 	 {
-	   fprintf(file, "%d ", I->item[v].label);
+	   fprintf(file, "%d ", I->item[v].label+1);
 	 }
      }
 
